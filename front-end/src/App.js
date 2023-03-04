4faf6b25b451f6645b5a21efb6f1e0892759fc9f
Isaiah Levy <isaiahlevy@10-16-216-229.dynapool.wireless.nyu.edu>
2023-03-04 18:27:17 -0500

    push in order to pull

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+//import logo from './logo.svg';
 import './App.css';
+//import Login from './Login'
+import SearchBar from './search_bar';
+
+let products = ['test'];
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        
+        <main className="App-main">
+          <Routes>
+            {/* a route for the home page */}
+            {/* <Route path="/" element={<Home />} /> */}
+
+            {/* a route for the Login page */}
+            <Route path="/" element={<SearchBar Products={products}/>} />
+
+            
+
+            
+          </Routes>
+        </main>
+       
+      </Router>
     </div>
   );
 }
