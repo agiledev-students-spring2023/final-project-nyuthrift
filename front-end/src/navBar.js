73a7fb207843474593bebf3a8361b28ad259e101
IsaiahLevy <il1016@nyu.edu>
2023-04-03 10:57:56 -0400

    updated-home

--- front-end/src/navBar.js
+++ front-end/src/navBar.js
@@ -6,11 +6,11 @@ function Navbar() {
   return (
     <nav className='Navbar'>
       <ul>
-        <li><NavLink to="/home" activeClassName="active">Home</NavLink></li>
+        <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
         <li><a href="/myprofile">MyProfile</a></li>
         <li><NavLink to="/messages" activeClassName="active">Messages</NavLink></li>
         <li><NavLink to="/sell" activeClassName="active">Sell</NavLink></li>
-        <li><NavLink to="/all" activeClassName="active">Shop All</NavLink></li>
+        
       </ul>
     </nav>
   );
