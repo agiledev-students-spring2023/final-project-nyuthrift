239d30da2f6203d771193d52db83b88013323782
mathewpan2 <mathew.pan2@gmail.com>
2023-05-02 19:43:10 -0400

    Revert back

--- front-end/src/chat/chatwindow.js
+++ front-end/src/chat/chatwindow.js
@@ -1,28 +1,101 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { styled } from '@mui/system';
+import {
+  Paper,
+  Grid,
+  Typography,
+  TextField,
+  Button,
+  Stack,
+} from '@mui/material';
 
-function ChatWindow({ currentUserId }) {
+const ChatWindow = styled(Paper)(({ theme }) => ({
+  height: '100vh',
+  marginTop: theme.spacing(2),
+  display: 'flex',
+  flexDirection: 'column-reverse',
+  position: 'fixed',
+  bottom: 0,
+  left: 0,
+  right: 0,
+  backgroundColor: '#f5f5f5',
+  boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.3)',
+  overflowY: 'scroll',
+  '&::-webkit-scrollbar': {
+    width: '8px',
+    backgroundColor: '#f5f5f5',
+  },
+  '&::-webkit-scrollbar-thumb': {
+    borderRadius: '10px',
+    backgroundColor: '#888',
+  },
+}));
+
+const Message = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(1),
+  borderRadius: '10px',
+  marginBottom: theme.spacing(1),
+  display: 'flex',
+  justifyContent: 'flex-start',
+}));
+
+const Received = styled(Grid)(({ theme }) => ({
+  marginTop: theme.spacing(1),
+  flexDirection: 'row',
+  alignSelf: 'flex-start',
+  maxWidth: '60%',
+  wordWrap: 'break-word',
+}));
+
+const Sent = styled(Grid)(({ theme }) => ({
+  marginBottom: theme.spacing(1),
+  flexDirection: 'row-reverse',
+  alignSelf: 'flex-end',
+  maxWidth: '60%',
+  wordWrap: 'break-word',
+}));
+const Form = styled('form')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'flex-end',
+  backgroundColor: '#f5f5f5',
+  padding: theme.spacing(1),
+}));
+
+const Input = styled(TextField)(({ theme }) => ({
+  flexGrow: 1,
+  marginRight: theme.spacing(1),
+}));
+
+const SendButton = styled(Button)(({ theme }) => ({
+  minWidth: 'auto',
+  padding: theme.spacing(1),
+  backgroundColor: '#2980b9',
+  color: '#fff',
+  '&:hover': {
+    backgroundColor: '#1a5276',
+  },
+}));
+
+
+function ChatWindowComponent({ currentUserId }) {
   const location = useLocation();
   const conversationId = location.state.conversationId;
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
-  
 
-  const token = localStorage.getItem('token'); // Get the JWT token from local storage
+  const token = localStorage.getItem('token');
 
   useEffect(() => {
-    
     async function fetchMessages() {
       try {
-        console.log(token)
-        
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/messages/${conversationId}`, {
-          headers: {
-            userId: currentUserId// Include the JWT token in the headers
-          },
-        });
+        const response = await fetch(`http://localhost:3000/api/messages/${conversationId}`,
+          {
+            headers: {
+              userId: currentUserId,
+            },
+          });
         const data = await response.json();
-        console.log(data)
         setMessages(data.messages);
       } catch (error) {
         console.error('Error fetching messages:', error);
@@ -38,51 +111,67 @@ function ChatWindow({ currentUserId }) {
     setMessage(e.target.value);
   };
 
-
-
   async function handleSubmit(e) {
     e.preventDefault();
-    console.log('Conversation ID in ChatWindow:', conversationId);
-    console.log('Submitting message:', message);
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/messages/${conversationId}`, {
+      const response = await fetch(`http://localhost:3000/api/messages/${conversationId}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`, // Include the JWT token in the headers
+          Authorization: `Bearer ${token}`,
         },
-  
-        body: JSON.stringify({ content: message, user: currentUserId }),
-      });
-
-      const newMessage = await response.json();
-      setMessages([...messages, newMessage]);
-      console.log('Message submitted:', response);
-    } catch (error) {
-      console.error('Error submitting message:', error);
-    }
-
+        body: JSON.stringify({ content: message, user_id: currentUserId,
+      }),
+    });
+    const data = await response.json();
+    setMessages((prevMessages) => [...prevMessages, data.message]);
     setMessage('');
+  } catch (error) {
+    console.error('Error sending message:', error);
   }
+}
 
-  return (
-    <div>
-      {messages.map((msg) => (
-        <div key={msg._id} className={msg.sender === currentUserId ? 'sent' : 'received'}>
-          <p>{msg.content}</p>
-        </div>
-      ))}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={message}
-          onChange={handleMessageChange}
-          placeholder="Type your message..."
-        />
-        <button type="submit">Send</button>
-      </form>
-    </div>
-  );
+return (
+  <ChatWindow>
+    <Stack spacing={2}>
+    {messages.map((msg) => (
+  <Grid
+    container
+    item
+    component={msg.user_id === currentUserId ? Sent : Received}
+    key={msg.id}
+    direction="row"
+    justifyContent={
+      msg.user_id === currentUserId ? 'flex-end' : 'flex-start'
+    }
+  >
+    <Grid item>
+      <Typography variant="body1">
+        {msg.user_id === currentUserId ? 'You' : msg.username}
+      </Typography>
+    </Grid>
+    <Grid item>
+      <Typography variant="body2">{msg.content}</Typography>
+    </Grid>
+  </Grid>
+))}
+
+    </Stack>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        label="Type your message"
+        variant="outlined"
+        size="small"
+        value={message}
+        onChange={handleMessageChange}
+      />
+      <SendButton type="submit" variant="contained">
+        Send
+      </SendButton>
+    </Form>
+  </ChatWindow>
+);
+
 }
 
 export default ChatWindow;
