43b5dbb70d7f8e4d1433ab798fd5256ebf71e8a5
mathewpan2 <mathew.pan2@gmail.com>
2023-05-02 15:58:47 -0400

    Revert "quick conversation fix"
    This reverts commit 0639e8b498a365f0c3330096a7f4fd0051f38b9f.

--- front-end/src/chat/Conversations.js
+++ front-end/src/chat/Conversations.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import ChatWindow from './chatwindow';
+import ChatWindow from './ChatWindow';
 import './Conversations.css';
 
 const Conversations = ({ conversations = [], currentUserId }) => {
