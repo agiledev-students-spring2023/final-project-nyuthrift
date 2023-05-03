a2fb6341be79c6d7f181c1d9575a4937f270a41b
XIAOiwnl <kefan0308@gmail.com>
2023-05-02 20:53:04 -0400

    Revert things

--- front-end/src/ProfileList.js
+++ front-end/src/ProfileList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Conversations from './chat/Conversations';
+import './ProfileList.css';
 
 const ProfileList = ({ conversations, userId }) => {
   console.log('ProfileList User Id: ', userId);
